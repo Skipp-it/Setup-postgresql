a366be871cf75b84a2f22197948f77f0af1fd745
Skipp-it <mihairaducroitoru@gmail.com>
2020-04-22 16:51:02 +0300

    dinos

---
+++ dinos-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO dinos VALUES ('Apatosaurus',5,23);
+INSERT INTO dinos VALUES ('Argentinosaurus',21,37);
+INSERT INTO dinos VALUES ('Branchiosaurus',15,31);
+INSERT INTO dinos VALUES ('Ceratosaurus',4,6);
+INSERT INTO dinos VALUES ('Megalosaurus',4,8);
+INSERT INTO dinos VALUES ('Oviraptor',1,2);
+INSERT INTO dinos VALUES ('Tyrannosaurus',7,15);
+INSERT INTO dinos VALUES ('Velociraptor',1,2);
---
+++ dinos-longer-10.sql
@@ -0,0 +1,1 @@
+SELECT name FROM dinos WHERE length >10;
